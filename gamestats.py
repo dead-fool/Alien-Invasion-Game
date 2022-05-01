74d9fd91a9043548a3d3274da97c2110f1129789
unknown <bibekniroula987@mail.com>
2022-05-01 11:46:21 +0545

    Highscores storage

--- gamestats.py
+++ gamestats.py
@@ -6,9 +6,20 @@ class GameStats:
         self.reset_stats()
 
         self.game_active = False
-        self.high_score = 0
+        self._get_high_score()
 
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def _get_high_score(self):
+        try:
+            with open('highscores.txt', 'r') as file_h:
+                for line in file_h:
+                    self.high_score = int(line.strip())
+                    print(self.high_score)
+        except FileNotFoundError:
+            self.high_score = 0
+            with open('highscores.txt', 'w') as file_h:
+                file_h.write(str(self.high_score))
